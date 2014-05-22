94194015f8e6ad4cf520947a134a74893c6bd4e5
bguiz <brendan.graetz@gmail.com>
2014-05-22 22:23:54 +1000

    =BG= ensure CNAME files does not get deleted during publish

--- publish.sh
+++ publish.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 TGT="../angular-ember-compare-publish"
+CD=$(pwd)
 
 rm -rf dist/static
 rm -rf ${TGT}/*
+cd ${TGT}
+git checkout -- CNAME
+cd ${CD}
+
 ./build.sh
 cp -r dist/static/* ${TGT}/
-rm -rf ${TGT}/gimp
+rm -rf ${TGT}/{gimp,_book}
+
 mkdir ${TGT}/pdf
 cp -r dist/pdf/index.pdf ${TGT}/pdf/angularjs-emberjs-compare.bguiz.com.pdf
+
