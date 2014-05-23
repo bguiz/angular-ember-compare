1ba71bf489b7489359e439bdbc62ba73be902157
bguiz <brendan.graetz@gmail.com>
2014-05-23 23:56:46 +1000

    =BG= hard code google analytics token for now

--- gitbook/plugins/gitbook-plugin-ga/plugin.js
+++ gitbook/plugins/gitbook-plugin-ga/plugin.js
@@ -2,12 +2,13 @@
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-37884921-4', 'bguiz.com');
+ga('send', 'pageview');
 
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("start", function(e, config) {
         config.ga = config.ga || {};
 
-        ga('create', config.ga.token, 'bguiz.com');
         ga('send', 'pageview');
     });
 
