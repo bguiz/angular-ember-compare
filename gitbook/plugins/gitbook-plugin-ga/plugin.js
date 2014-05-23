5e2db640f0020d4ce46fec6bd0db47010f79a4f0
bguiz <brendan.graetz@gmail.com>
2014-05-24 01:44:38 +1000

    =BG= upgrade to gitbook-plugin-ga such that script tag is appended to each page inline (rather than via javascript)

--- gitbook/plugins/gitbook-plugin-ga/plugin.js
+++ gitbook/plugins/gitbook-plugin-ga/plugin.js
@@ -1,15 +1,6 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-37884921-4', 'bguiz.com');
-ga('send', 'pageview');
-
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("start", function(e, config) {
         config.ga = config.ga || {};
-
-        ga('send', 'pageview');
     });
 
     gitbook.events.bind("page.change", function() {
@@ -17,6 +8,6 @@ require(["gitbook"], function(gitbook) {
     });
 
     gitbook.events.bind("exercise.submit", function(e, data) {
-        
+
     });
 });
