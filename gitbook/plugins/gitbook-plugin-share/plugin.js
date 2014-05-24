4231238079bb112eece2c7c26f9a89c268c095f5
bguiz <brendan.graetz@gmail.com>
2014-05-24 21:09:39 +1000

    =BG= make reddit share configurable, and use it to open in new window

--- gitbook/plugins/gitbook-plugin-share/plugin.js
+++ gitbook/plugins/gitbook-plugin-share/plugin.js
@@ -1,7 +1,7 @@
 require(["gitbook"], function(gitbook) {
 
 	var redditButton = '<script type="text/javascript" src="http://www.reddit.com/static/button/button1.js"></script>';
-	var redditButtonCreate = function() {
+	var redditButtonCreate = function(cfg) {
 	  if ('https:' == document.location.protocol) {
 	      var base_url = 'https://redditstatic.s3.amazonaws.com'
 	  } else {
@@ -10,29 +10,30 @@ require(["gitbook"], function(gitbook) {
 
 	  var write_string="<iframe src=\"" + base_url + "/button/button1.html?width=120&url=";
 
-	  if (window.reddit_url)  { 
+	  if (cfg.url)  { 
 	      write_string += encodeURIComponent(reddit_url); 
 	  }
 	  else { 
 	      write_string += encodeURIComponent(window.location.href);
 	  }
-	  if (window.reddit_title) {
-	       write_string += '&title=' + encodeURIComponent(window.reddit_title);
+	  if (cfg.title) {
+	       write_string += '&title=' + encodeURIComponent(cfg.title);
 	  }
-	  if (window.reddit_target) {
-	       write_string += '&sr=' + encodeURIComponent(window.reddit_target);
+	  if (cfg.target) {
+	       write_string += '&sr=' + encodeURIComponent(cfg.target);
 	  }
-	  if (window.reddit_css) {
-	      write_string += '&css=' + encodeURIComponent(window.reddit_css);
+	  if (cfg.css) {
+	      write_string += '&css=' + encodeURIComponent(cfg.css);
 	  }
-	  if (window.reddit_bgcolor) {
-	      write_string += '&bgcolor=' + encodeURIComponent(window.reddit_bgcolor); 
+	  if (cfg.bgcolor) {
+	      write_string += '&bgcolor=' + encodeURIComponent(cfg.bgcolor); 
 	  }
-	  if (window.reddit_bordercolor) {
-	      write_string += '&bordercolor=' + encodeURIComponent(window.reddit_bordercolor); 
+	  if (cfg.bordercolor) {
+	      write_string += '&bordercolor=' + encodeURIComponent(cfg.bordercolor); 
 	  }
-	  if (window.reddit_newwindow) { 
-	      write_string += '&newwindow=' + encodeURIComponent(window.reddit_newwindow);}
+	  if (cfg.newwindow) { 
+	      write_string += '&newwindow=' + encodeURIComponent(cfg.newwindow);
+	  }
 	  write_string += "\" height=\"22\" width=\"120\" scrolling='no' frameborder='0'></iframe>";
 	  // document.write(write_string);
 	  return write_string;
@@ -42,7 +43,7 @@ require(["gitbook"], function(gitbook) {
 	var reloadShare = function() {
         if (shareConfig.reddit) {
             // var out = '<script type="text/javascript" src="http://www.reddit.com/static/button/button1.js"></script>';
-        	var out = redditButtonCreate();
+        	var out = redditButtonCreate(shareConfig.reddit);
         	$(".gitbook-share").append($(out));
         }
 	};
