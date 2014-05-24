d53109e26824e93bb260e84e6289a53819878c19
bguiz <brendan.graetz@gmail.com>
2014-05-24 18:48:12 +1000

    =BG= test tweet button

---
+++ gitbook/plugins/gitbook-plugin-share/plugin.js
@@ -0,0 +1,10 @@
+require(["gitbook"], function(gitbook) {
+    gitbook.events.bind("start", function(e, config) {
+    });
+
+    gitbook.events.bind("page.change", function() {
+    });
+
+    gitbook.events.bind("exercise.submit", function(e, data) {
+    });
+});
